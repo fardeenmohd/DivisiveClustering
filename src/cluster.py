8bf2e659eadbcd2838cbc4a14822112cac6b53d8
matrackif <filip.matracki@gmail.com>
2017-12-12 22:12:30 +0100

    added initial implementation of regular k means clustering

--- src/cluster.py
+++ src/cluster.py
@@ -3,6 +3,8 @@ from typing import List
 from typing import Dict
 import operator
 from copy import deepcopy
+from math import sqrt
+import random
 
 
 class Cluster:
@@ -10,12 +12,15 @@ class Cluster:
     def __init__(self, matches: List[DotaMatch] = None, hero_names: Dict[int, str] = None):
         self.matches = matches
         self.hero_names = hero_names
-        self.center = self.get_center()
+        self.center = None
+        self.update_center()
 
-    def get_center(self) -> DotaMatch:
+    def update_center(self):
+        if len(self.matches) == 0:
+            self.center = None
 
         num_of_matches = len(self.matches)
-        avg_match = deepcopy(self.matches[0])
+        self.center = deepcopy(self.matches[0])
         victorious_hero_count = {}  # From hero_id -> number of heroes
         lost_hero_count = {}  # From hero_id -> number of heroes
 
@@ -23,16 +28,16 @@ class Cluster:
             victorious_hero_count[hero_id] = 0
             lost_hero_count[hero_id] = 0
 
-        for hero in avg_match.heroes_that_won:
+        for hero in self.center.heroes_that_won:
             victorious_hero_count[hero] += 1
 
-        for hero in avg_match.heroes_that_lost:
+        for hero in self.center.heroes_that_lost:
             lost_hero_count[hero] += 1
 
         for i in range(1, num_of_matches):
-            avg_match.avg_mmr += self.matches[i].avg_mmr
-            avg_match.avg_rank_tier += self.matches[i].avg_rank_tier
-            avg_match.duration += self.matches[i].duration
+            self.center.avg_mmr += self.matches[i].avg_mmr
+            self.center.avg_rank_tier += self.matches[i].avg_rank_tier
+            self.center.duration += self.matches[i].duration
             for hero in self.matches[i].heroes_that_won:
                 victorious_hero_count[hero] += 1
             for hero in self.matches[i].heroes_that_lost:
@@ -47,13 +52,13 @@ class Cluster:
         most_common_lost_heroes = sorted_lost_hero_counts[-5:]
         most_common_lost_heroes = [x[0] for x in most_common_lost_heroes]
 
-        avg_match.heroes_that_won = most_common_winning_heroes
-        avg_match.heroes_that_lost = most_common_lost_heroes
-        avg_match.avg_mmr = int(avg_match.avg_mmr / num_of_matches)
-        avg_match.avg_rank_tier = int(avg_match.avg_rank_tier / num_of_matches)
-        avg_match.duration = int(avg_match.duration / num_of_matches)
+        self.center.heroes_that_won = most_common_winning_heroes
+        self.center.heroes_that_lost = most_common_lost_heroes
+        self.center.avg_mmr = int(self.center.avg_mmr / num_of_matches)
+        self.center.avg_rank_tier = int(self.center.avg_rank_tier / num_of_matches)
+        self.center.duration = int(self.center.duration / num_of_matches)
 
-        return avg_match
+        return deepcopy(self.center)
 
     def print_center(self):
         print('----------Cluster center information----------')
@@ -64,3 +69,76 @@ class Cluster:
         print('Avg MMR: ' + str(self.center.avg_mmr))
         print('Avg rank tier: ' + str(self.center.avg_rank_tier))
         print('Avg duration: ' + str(self.center.duration))
+
+
+def get_distance(d1: DotaMatch, d2: DotaMatch) -> int:
+    euclid_dist = sqrt((d1.avg_mmr - d2.avg_mmr) ** 2 + (d1.avg_rank_tier - d2.avg_rank_tier) ** 2 +
+                       (d1.duration - d2.duration) ** 2)
+    hero_weight = 0.05
+    hero_value = hero_weight * euclid_dist
+
+    for i in range(len(d1.heroes_that_won)):
+        for j in range(len(d2.heroes_that_won)):
+            if d1.heroes_that_won[i] == d2.heroes_that_won[j]:
+                # print('About to subtract hero value of: ' + str(hero_value) + ' from: ' + str(euclid_dist))
+                euclid_dist -= hero_value
+            if d1.heroes_that_lost[i] == d2.heroes_that_lost[j]:
+                # print('About to subtract hero value of: ' + str(hero_value) + ' from: ' + str(euclid_dist))
+                euclid_dist -= hero_value
+    return int(euclid_dist)
+
+
+def get_distance_to_cluster(d1: DotaMatch, c1: Cluster) -> int:
+    euclid_dist = sqrt((d1.avg_mmr - c1.center.avg_mmr) ** 2 + (d1.avg_rank_tier - c1.center.avg_rank_tier) ** 2 +
+                       (d1.duration - c1.center.duration) ** 2)
+    hero_weight = 0.05
+    hero_value = hero_weight * euclid_dist
+
+    for i in range(len(d1.heroes_that_won)):
+        for j in range(len(c1.center.heroes_that_won)):
+            if d1.heroes_that_won[i] == c1.center.heroes_that_won[j]:
+                # print('About to subtract hero value of: ' + str(hero_value) + ' from: ' + str(euclid_dist))
+                euclid_dist -= hero_value
+            if d1.heroes_that_lost[i] == c1.center.heroes_that_lost[j]:
+                # print('About to subtract hero value of: ' + str(hero_value) + ' from: ' + str(euclid_dist))
+                euclid_dist -= hero_value
+    return int(euclid_dist)
+
+
+def k_means(matches: List[DotaMatch], hero_names_dict: Dict[int, str], num_of_clusters: int = 3, max_iters: int = 100) -> List[Cluster]:
+    num_of_matches = len(matches)
+    initial_cluster_indices = random.sample(range(num_of_matches), num_of_clusters)
+    final_clusters = []
+    # Create initial clusters with seed
+    for idx in initial_cluster_indices:
+        final_clusters.append(Cluster(matches=[matches[idx]], hero_names=hero_names_dict))
+    # Assign all matches randomly to a cluster
+    for idx in range(num_of_matches):
+        if idx not in initial_cluster_indices:
+            rand_idx = random.randint(0, num_of_clusters - 1)
+            final_clusters[rand_idx].matches.append(matches[idx])
+
+    i = 0
+    has_changed = True
+
+    while i < max_iters and has_changed:
+        has_changed = False
+        for cluster in final_clusters:
+            cluster.update_center()
+        for i in range(len(final_clusters)):
+            j = 0
+            while j < len(final_clusters[i].matches):
+                match = final_clusters[i].matches[j]
+                distances = []
+                for k in range(len(final_clusters)):
+                    distances.append(get_distance_to_cluster(match, final_clusters[k]))
+                closest_cluster_idx = distances.index(min(distances))
+                if closest_cluster_idx != i and len(final_clusters[i].matches) > 1:
+                    final_clusters[i].matches.remove(match)
+                    final_clusters[closest_cluster_idx].matches.append(match)
+                    j -= 1
+                    has_changed = True
+                j += 1
+        i += 1
+    print('Clustering finished in ' + str(i + 1) + ' iterations, max num of iters: ' + str(max_iters))
+    return final_clusters
--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 import requests
 import json
 from dota_match import DotaMatch
-from cluster import Cluster
+import cluster
 API = 'https://api.opendota.com/api/'
 PUBLIC_MATCHES = 'publicMatches'
 HEROES = 'heroes'
 MATCHES = 'matches'
+
+
+def get_hero_names():
+    list_of_hero_dicts = json.loads(heroes_response.text)
+    hero_names_dictionary = dict()
+    for hero_dict in list_of_hero_dicts:
+        hero_names_dictionary[hero_dict['id']] = hero_dict['localized_name']
+
+    print('Hero id and names: ' + str(hero_names_dictionary))
+    return hero_names_dictionary
 
 if __name__ == '__main__':
 
@@ -13,21 +23,25 @@ if __name__ == '__main__':
     heroes_response = requests.get(API + HEROES)
 
     list_of_match_dicts = json.loads(matches_response.text)
-    list_of_hero_dicts = json.loads(heroes_response.text)
-
-    hero_names_dict = dict()
     list_of_dota_matches = list()
-
-    for hero_dict in list_of_hero_dicts:
-        hero_names_dict[hero_dict['id']] = hero_dict['localized_name']
-
-    print('Hero id and names: ' + str(hero_names_dict))
+    hero_names_dict = get_hero_names()
 
     for match_dict in list_of_match_dicts:
         if match_dict['avg_mmr'] is not None:
             match = DotaMatch(match_dict, hero_names_dict)
             list_of_dota_matches.append(match)
             print(str(match))
-
-    initial_cluster = Cluster(matches=list_of_dota_matches, hero_names=hero_names_dict)
+    print('Query returned ' + str(len(list_of_dota_matches)) + ' matches')
+    initial_cluster = cluster.Cluster(matches=list_of_dota_matches, hero_names=hero_names_dict)
     initial_cluster.print_center()
+    # Get disjoint pair distances
+    for i in range(len(initial_cluster.matches)):
+        for j in range(i, len(initial_cluster.matches)):
+            if i != j:
+                print('Distance is: ' + str(cluster.get_distance(initial_cluster.matches[i], initial_cluster.matches[j])))
+
+    k_means_clusters = cluster.k_means(list_of_dota_matches, hero_names_dict)
+    print('Found clusters for k=' + str(len(k_means_clusters)) + ' about to print centers...')
+    for cluster in k_means_clusters:
+        print('Number of matches in cluster: ' + str(len(cluster.matches)))
+        cluster.print_center()
