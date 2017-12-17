347d298b6755b2bdc51ba3f5ab78aa281779eeaf
matrackif <filip.matracki@gmail.com>
2017-12-17 19:28:36 +0100

    Implemented initial version of divisive Clustering with k-means re-clustering

--- src/clustering.py
+++ src/clustering.py
@@ -1,6 +1,5 @@
 from dota_match import DotaMatch
-from typing import List
-from typing import Dict
+from typing import List, Dict, Tuple, Union
 import operator
 from copy import deepcopy
 from math import sqrt
@@ -8,7 +7,6 @@ import random
 
 
 class Cluster:
-
     def __init__(self, matches: List[DotaMatch] = None, hero_names: Dict[int, str] = None):
         self.matches = matches
         self.hero_names = hero_names
@@ -18,6 +16,7 @@ class Cluster:
     def update_center(self):
         if len(self.matches) == 0:
             self.center = None
+            return
 
         num_of_matches = len(self.matches)
         self.center = deepcopy(self.matches[0])
@@ -105,18 +104,21 @@ def get_distance_to_cluster(d1: DotaMatch, c1: Cluster) -> int:
     return int(euclid_dist)
 
 
-def k_means(matches: List[DotaMatch], hero_names_dict: Dict[int, str], num_of_clusters: int = 3, max_iters: int = 100) -> List[Cluster]:
-    num_of_matches = len(matches)
-    initial_cluster_indices = random.sample(range(num_of_matches), num_of_clusters)
-    final_clusters = []
-    # Create initial clusters with seed
-    for idx in initial_cluster_indices:
-        final_clusters.append(Cluster(matches=[matches[idx]], hero_names=hero_names_dict))
-    # Assign all matches randomly to a cluster
-    for idx in range(num_of_matches):
-        if idx not in initial_cluster_indices:
-            rand_idx = random.randint(0, num_of_clusters - 1)
-            final_clusters[rand_idx].matches.append(matches[idx])
+def k_means(hero_names_dict: Dict[int, str], num_of_clusters: int = 3,
+            max_iters: int = 100, matches: List[DotaMatch] = None, final_clusters: List[Cluster] = None) -> List[Cluster]:
+
+    if final_clusters is None:
+        final_clusters = []
+        num_of_matches = len(matches)
+        initial_cluster_indices = random.sample(range(num_of_matches), num_of_clusters)
+        # Create initial clusters with seed
+        for idx in initial_cluster_indices:
+            final_clusters.append(Cluster(matches=[matches[idx]], hero_names=hero_names_dict))
+        # Assign all matches randomly to a cluster
+        for idx in range(num_of_matches):
+            if idx not in initial_cluster_indices:
+                rand_idx = random.randint(0, num_of_clusters - 1)
+                final_clusters[rand_idx].matches.append(matches[idx])
 
     i = 0
     has_changed = True
@@ -140,5 +142,55 @@ def k_means(matches: List[DotaMatch], hero_names_dict: Dict[int, str], num_of_cl
                     has_changed = True
                 j += 1
         i += 1
-    print('Clustering finished in ' + str(i + 1) + ' iterations, max num of iters: ' + str(max_iters))
+    print('Clustering finished in ' + str(i) + ' iterations, max num of iters: ' + str(max_iters))
     return final_clusters
+
+
+def split_cluster(cluster_to_be_split: Cluster, hero_names_dict: Dict[int, str], max_dist: int = 1000) -> Union[Tuple[Cluster, Cluster], None]:
+    new_clusters = Cluster(matches=[], hero_names=hero_names_dict), Cluster(matches=[], hero_names=hero_names_dict)
+    max_found_dist = 0
+    num_of_matches = len(cluster_to_be_split.matches)
+
+    if num_of_matches == 1:
+        print('split_cluster(): Cluster has only 1 element, returning None')
+        return None
+
+    farthest_match_pair = 0, 0
+
+    # Get disjoint pair distances
+    for i in range(num_of_matches):
+        for j in range(i + 1, num_of_matches):
+            dist = get_distance(cluster_to_be_split.matches[i], cluster_to_be_split.matches[j])
+            if dist > max_found_dist:
+                farthest_match_pair = i, j
+                max_found_dist = dist
+
+    if max_found_dist < max_dist:
+        print('split_cluster(): max_found_dist of ' + str(max_found_dist) + ' is less than max_dist of: '
+              + str(max_dist) + ', returning None')
+        # We only split the cluster if the maximum distance between pairs is greater than a constant
+        return None
+
+    for i in range(num_of_matches):
+        match = cluster_to_be_split.matches[i]
+        if get_distance(match, cluster_to_be_split.matches[farthest_match_pair[0]]) < \
+                get_distance(match, cluster_to_be_split.matches[farthest_match_pair[1]]):
+            # cluster_to_be_split.matches.remove(match)
+            new_clusters[0].matches.append(match)
+        else:
+            # cluster_to_be_split.matches.remove(match)
+            new_clusters[1].matches.append(match)
+
+    new_clusters[0].update_center()
+    new_clusters[1].update_center()
+    return new_clusters
+
+
+def run_divisive_clustering(cluster: Cluster, hero_names_dict: Dict[int, str], max_dist: int, final_clusters: List[Cluster]):
+    new_clusters = split_cluster(cluster, hero_names_dict, max_dist)
+    if new_clusters is None:
+        # We could not split the input cluster so it is one of our final clusters
+        final_clusters.append(cluster)
+    else:
+        run_divisive_clustering(new_clusters[0], hero_names_dict, max_dist, final_clusters)
+        run_divisive_clustering(new_clusters[1], hero_names_dict, max_dist, final_clusters)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import requests
 import json
 from dota_match import DotaMatch
-import cluster
+import clustering
+import time
+
 API = 'https://api.opendota.com/api/'
 PUBLIC_MATCHES = 'publicMatches'
 HEROES = 'heroes'
@@ -16,6 +18,7 @@ def get_hero_names():
 
     print('Hero id and names: ' + str(hero_names_dictionary))
     return hero_names_dictionary
+
 
 if __name__ == '__main__':
 
@@ -32,16 +35,58 @@ if __name__ == '__main__':
             list_of_dota_matches.append(match)
             print(str(match))
     print('Query returned ' + str(len(list_of_dota_matches)) + ' matches')
-    initial_cluster = cluster.Cluster(matches=list_of_dota_matches, hero_names=hero_names_dict)
-    initial_cluster.print_center()
-    # Get disjoint pair distances
-    for i in range(len(initial_cluster.matches)):
-        for j in range(i, len(initial_cluster.matches)):
-            if i != j:
-                print('Distance is: ' + str(cluster.get_distance(initial_cluster.matches[i], initial_cluster.matches[j])))
 
-    k_means_clusters = cluster.k_means(list_of_dota_matches, hero_names_dict)
-    print('Found clusters for k=' + str(len(k_means_clusters)) + ' about to print centers...')
+    initial_cluster = clustering.Cluster(matches=list_of_dota_matches, hero_names=hero_names_dict)
+    initial_cluster.print_center()
+    max_dist = 0
+    num_of_matches = len(initial_cluster.matches)
+    farthest_cluster_pair = 0, 0
+    avg_dist = 0
+    num_of_disjoint_pairs = 0
+    # Get disjoint pair distances
+    for i in range(num_of_matches):
+        for j in range(i + 1, num_of_matches):
+            dist = clustering.get_distance(initial_cluster.matches[i], initial_cluster.matches[j])
+            avg_dist += dist
+            num_of_disjoint_pairs += 1
+            if dist > max_dist:
+                max_dist = dist
+                farthest_cluster_pair = i, j
+            print('Distance is: ' + str(dist))
+
+    avg_dist = (avg_dist / num_of_disjoint_pairs)
+    print('Average distance between Dota matches: ' + str(avg_dist) + ' max dist is: ' + str(max_dist))
+    divisive_clustering_clusters = []
+    divisive_start_time = time.time()
+    clustering.run_divisive_clustering(initial_cluster, hero_names_dict, int(avg_dist), divisive_clustering_clusters)
+    divisive_end_time = time.time()
+    suggested_value_of_k = len(divisive_clustering_clusters)
+
+    print('Num of clusters found in divisive clustering: ' + str(
+        len(divisive_clustering_clusters)) + ' Time of computation: %s seconds. About to print centers...' % (
+          divisive_end_time - divisive_start_time))
+    for cluster in divisive_clustering_clusters:
+        print('Number of matches in cluster: ' + str(len(cluster.matches)))
+        cluster.print_center()
+
+    reclustering_start_time = time.time()
+    k_means_clusters = clustering.k_means(hero_names_dict=hero_names_dict, num_of_clusters=suggested_value_of_k,
+                                          final_clusters=divisive_clustering_clusters)
+    reclustering_end_time = time.time()
+    print('K-Means with re-clustering found clusters for k=' + str(
+        len(k_means_clusters)) + ' In %s seconds. About to print centers...' % (
+          reclustering_end_time - reclustering_start_time))
     for cluster in k_means_clusters:
+        print('Number of matches in cluster: ' + str(len(cluster.matches)))
+        cluster.print_center()
+
+    random_start_time = time.time()
+    k_means_random_clusters = clustering.k_means(hero_names_dict=hero_names_dict, num_of_clusters=suggested_value_of_k,
+                                                 matches=list_of_dota_matches)
+    random_end_time = time.time()
+    print('K-Means with random initialization found clusters for k=' + str(
+        len(k_means_random_clusters)) + ' In %s seconds. About to print centers...' % (
+          random_end_time - random_start_time))
+    for cluster in k_means_random_clusters:
         print('Number of matches in cluster: ' + str(len(cluster.matches)))
         cluster.print_center()
